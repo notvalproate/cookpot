48c733fc3ef09587162e3d69ec594a5a10eaf165
Ian Tauro <sushruth127@gmail.com>
2024-07-12 14:58:53 +0530

    Setup some basic stuff

--- website/src/main.jsx
+++ website/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+
 import './index.css';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <div>Hello world!</div>,
+        errorElement: <div>Error</div>
+    },
+    {
+        path: '/home',
+        element: <div>Home</div>
+    },
+    {
+        path: '/login',
+        element: <div>Login</div>
+    },
+    {
+        path: '/**',
+        element: <div>Not Found</div>
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <App />
+        <RouterProvider router={router}/>
     </React.StrictMode>
 );
